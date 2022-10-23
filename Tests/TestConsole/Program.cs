4e96943f1298389c7378e7231f0bc5326ce106d0
KuzoryaAky <opanforever1992@gmail.com>
2022-10-23 17:59:49 +0300

    Логика отправкисообщения

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Net;
+using System.Net.Mail;
 
 namespace TestConsole
 {
@@ -6,7 +7,25 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var from = new MailAddress("ya.aleksandrovich@retailrobotics.microsoft.com", "Отправляю сообщение с рабочей почты");
+            var to = new MailAddress("opanforever1992@gmail.com", "Yarosav это ты");
+
+            var message = new MailMessage(from, to);
+            message.Subject = "Заголовок";
+            message.Body = "Текст письма";
+
+
+            var client = new SmtpClient("smtp.yandex.ru", 465);
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential 
+            {
+                UserName = "UserName",
+                //SecurePassword = 
+                Password = "Password"
+            };
+
+            client.Send(message);
         }
     }
 }
