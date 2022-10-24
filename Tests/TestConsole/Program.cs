cb28ec6ca9862d15dfac2169fc3ac2c8e253304e
opanforever1992 <opanforever1992@gmail.com>
2022-10-24 10:32:25 +0300

    Добавление Ядра программы отправления сообщения с гугла на яндекс

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Mail;
+using System.Net;
 
 namespace TestConsole
 {
@@ -6,7 +8,25 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // отправитель - устанавливаем адрес и отображаемое в письме имя
+            MailAddress from = new MailAddress("opanforever1992@gmail.com", "Yaroslav");
+            // кому отправляем
+            MailAddress to = new MailAddress("dcaxarokb@yandex.ru");
+            // создаем объект сообщения
+            MailMessage m = new MailMessage(from, to);
+            // тема письма
+            m.Subject = "Тест";
+            // текст письма
+            m.Body = "<h2>Письмо-тест работы smtp-клиента</h2>";
+            // письмо представляет код html
+            m.IsBodyHtml = true;
+            // адрес smtp-сервера и порт, с которого будем отправлять письмо
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            // логин и пароль
+            smtp.Credentials = new NetworkCredential("opanforever1992@gmail.com","");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+            Console.Read();
         }
     }
 }
--- Tests/TestWPF/MainWindow.xaml.cs
+++ Tests/TestWPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,54 @@ namespace TestWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            // отправитель - устанавливаем адрес и отображаемое в письме имя
+            MailAddress from = new MailAddress("opanforever1992@gmail.com", "Yaroslav");
+            // кому отправляем
+            MailAddress to = new MailAddress("dcaxarokb@yandex.ru");
+            // создаем объект сообщения
+            MailMessage m = new MailMessage(from, to);
+            
+            // тема письма
+            m.Subject = "Тест";
+            // текст письма
+            m.Body = "<h2>Письмо-тест работы smtp-клиента</h2>";
+            // письмо представляет код html
+            m.IsBodyHtml = true;
+            
+            // адрес smtp-сервера и порт, с которого будем отправлять письмо
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            
+            
+            // логин и пароль
+            smtp.Credentials = new NetworkCredential 
+            {
+                UserName = LoginEdit.Text,
+                //Password = PasswordEdit.Text
+                SecurePassword = PasswordEdit.SecurePassword
+            };
+            smtp.EnableSsl = true;
+
+
+            try
+            {
+                smtp.Send(m);
+
+                MessageBox.Show("Сообщение успешно отправленно!", "Отправка почты", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+            catch (SmtpException)
+            {
+                MessageBox.Show("Ошибка авторизации", "Ошибка отправки почты", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            catch (TimeoutException)
+            {
+                MessageBox.Show("Ошибка адреса сервера", "Ошибка отправки почты", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
     }
 }
