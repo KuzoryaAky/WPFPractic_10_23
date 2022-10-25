ef47d6aa99ded3c58d5a057c41e0677461ef6ed4
KuzoryaAky <opanforever1992@gmail.com>
2022-10-25 22:55:59 +0300

    Сервис отправки почты

---
+++ MeilSender.lib/MeilenderService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace MeilSender.lib
+{
+    public class MeilenderService
+    {
+        public string ServerAddress {get; set;}
+        public int ServerPort { get; set; }
+        public bool UseSSL { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public void SendMessage(string SenderAddress, string RecipientAddress, string Subject, string Body)
+        {
+            var from = new MailAddress(SenderAddress);
+            var to = new MailAddress(RecipientAddress);
+
+            using var message = new MailMessage(from, to)
+            {
+                Subject = Subject,
+                Body = Body
+            };
+
+            using var client = new SmtpClient(SenderAddress, ServerPort)
+            {
+                EnableSsl = UseSSL,
+                Credentials = new NetworkCredential
+                {
+                    UserName = Login,
+                    Password = Password
+                }
+            };
+
+            client.Send(message);
+        }
+    }
+}
